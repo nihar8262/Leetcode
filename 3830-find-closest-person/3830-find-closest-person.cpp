7ee27694b69fbdff68ff745574b5fcb95844d2f8
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2025-09-08 12:44:06 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.55%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.cpp
+++ 3830-find-closest-person/3830-find-closest-person.cpp
@@ -3,7 +3,6 @@ public:
     int findClosest(int x, int y, int z) {
         if(abs(x-z)<abs(y-z)) return 1;
         else if(abs(x-z)>abs(y-z)) return 2;
-        else if(abs(x-z) == abs(y-z)) return 0;
         return 0;
     }
 };
