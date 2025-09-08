2a9923c0d75c4938e96c62d8f5b015f4d2406758
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2025-09-08 12:35:53 +0530

    Time: 0 ms (100%), Space: 8.1 MB (85.27%) - LeetHub

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
     vector<int> getNoZeroIntegers(int n) {
-        int x = 0,num = n;
-        string val,valx;
-        bool flag = true,flagx =true;
-
-        while(num>1){
-            x++;
-            num = n - x;
-            val = to_string(num) ;
-            valx = to_string(x);
-            int i =0,j=0;
-            flag = true;
-            flagx = true;
-            while(i<val.size()){
-                if(val[i] == '0') {
-                    flag = false;
-                    break;
-                }
-                i++;
+        auto check = [](int x) {
+            while (x > 0) {
+                if (x % 10 == 0) return false;
+                x /= 10;
             }
-            while(j<valx.size()){
-                if(valx[j] == '0'){
-                    flagx = false;
-                    break;
-                }
-            cout<<valx[j]<<" "<<flagx<<endl;
-                j++;
+            return true;
+        };
+        for (int i = 1; i < n; ++i) {
+            int j = n - i;
+            if (check(i) && check(j)) {
+                return {i, j};
             }
-            if(x>0 &&  (flag == true && flagx == true))  return {x,num} ;
-            // cout<<"-----"<<endl;
-            // cout<<"-----"<<endl;
-
         }
-
-        return {x,num};
+        return {};
     }
 };
