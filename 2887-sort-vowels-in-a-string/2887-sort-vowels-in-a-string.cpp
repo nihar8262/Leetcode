dd779760de2ea41b6fc7a673ebd7732e63912240
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2025-09-15 15:30:36 +0530

    Time: 20 ms (52.25%), Space: 14.7 MB (98.36%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     bool isVowel(char ch) {
-        if ((ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') ||
-            (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'))
-            return true;
-        else
-            return false;
+        return string("AEIOUaeiou").find(ch) != string::npos;
     }
 
     string sortVowels(string s) {
-        int n = s.size(), k = 0;
-        string vowel = "";
+        string vowels = "AEIOUaeiou";
+        vector<int> freq(10, 0);
 
-        for(int i = 0; i<n; i++){
-            if(isVowel(s[i])) vowel += s[i];
+        
+        for(char c : s) {
+            int idx = vowels.find(c);
+            if(idx != string::npos) freq[idx]++;
         }
 
-        sort(vowel.begin(),vowel.end());
-        cout<<vowel<<endl;
-
-        for(int i = 0; i<n; i++){
+        
+        int vIndex = 0; 
+        for(int i = 0; i < s.size(); i++) {
             if(isVowel(s[i])) {
-                s[i] = vowel[k];
-                k++;
+                while(freq[vIndex] == 0) vIndex++;
+                s[i] = vowels[vIndex];
+                freq[vIndex]--;
             }
         }
 
         return s;
     }
 };
-
