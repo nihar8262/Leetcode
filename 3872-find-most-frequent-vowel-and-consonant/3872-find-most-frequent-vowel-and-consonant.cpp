56e5044734502a5b38dd9bb80cdfa408e100007f
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2025-09-15 15:42:23 +0530

    Time: 6 ms (6.83%), Space: 10.5 MB (7.83%) - LeetHub

---
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    bool isVowel(char ch) {
+        return string("AEIOUaeiou").find(ch) != string::npos;
+    }
+
+    int maxFreqSum(string s) {
+        int maxiV = 0, maxiC = 0;
+        unordered_map<char,int> mp;
+
+        for(char ch: s){
+            mp[ch]++;
+        }
+
+        int n = mp.size();
+
+        for(auto it:mp){
+            if(isVowel(it.first)){
+                if(it.second>maxiV) maxiV = it.second;
+            }else {
+                if(it.second>maxiC) maxiC = it.second;
+            }
+        }
+
+        return maxiC + maxiV ;
+    }
+};
