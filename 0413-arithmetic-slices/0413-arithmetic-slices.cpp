314684bf93ced1ebcd8b429bb8552cd390bc7c68
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2025-09-19 14:23:40 +0530

    Time: 0 ms (100%), Space: 10.8 MB (82.34%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.cpp
+++ 0413-arithmetic-slices/0413-arithmetic-slices.cpp
@@ -3,17 +3,23 @@ public:
     int numberOfArithmeticSlices(vector<int>& nums) {
          
         int n = nums.size();
+        if(n < 3) return 0;
+        int i=0;
+        int j=1;
+        int common_diff = nums[j]-nums[i];
+        int k=2;
+
         int count = 0;
 
-        for(int i=0; i<(n-2); i++){
-            int common_d = nums[i+1]-nums[i];
-            for(int j=i+2; j<n; j++){
-                if((nums[j]-nums[j-1]) == common_d) {
-                    count++;
-                }else{
-                    break;
-                }
+        while(k<n) {
+            while(k<n && ((nums[k] - nums[k-1]) == common_diff)) {
+                count += k-i-1;
+                k++;
             }
+            i = k-1;
+            j= k;
+            if(j<n)common_diff = nums[j]-nums[i];
+            k++;
         }
 
         return count;
