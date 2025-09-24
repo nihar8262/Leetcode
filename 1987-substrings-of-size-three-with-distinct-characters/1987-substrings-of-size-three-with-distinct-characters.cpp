264f3543be7b2e83e453cd432ee7f409cb48c7d1
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2025-09-24 12:49:19 +0530

    Time: 3 ms (27.2%), Space: 9.9 MB (14.34%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
@@ -8,17 +8,14 @@ public:
         while(i<n-2 && j<n){
             if(mp[s[j]] == 0 && k<3){
                 mp[s[j]]++;
-                cout<<s[j]<<" "<<mp[s[j]]<<endl;
                 k++;
                 j++;
                 if(k == 3) cnt++;
             }else {
-                cout<<"restart for j "<<j<<" " << s[j] <<endl;
                 i++;
                 j = i;
                 k=0;
                 mp.clear();
-                cout<<"map size: "<< mp.size()<<" "<<i<< endl;
             }
         }
 
