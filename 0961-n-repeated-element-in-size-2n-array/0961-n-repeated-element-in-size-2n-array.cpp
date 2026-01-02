cb780d1a97472d657a99b133b783ff838fc7ed69
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2026-01-02 12:51:58 +0530

    Time: 4 ms (44.28%), Space: 28.5 MB (69.74%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -12,15 +12,6 @@ public:
             else cnt = 0;
         }
         if(cnt == (n/2)-1) return nums[n-1];
-        // unordered_map<int, int> mp;
-
-        // for(int it: nums){
-        //     mp[it]++;
-        // }
-
-        // for(auto it: mp){
-        //     if(it.second == (n/2)) return it.first;
-        // }
 
         return 0;
     }
