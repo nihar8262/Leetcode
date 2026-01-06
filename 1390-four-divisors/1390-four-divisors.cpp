acc13321e74625ed5dde6e16be9c8007b3480858
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2026-01-06 14:24:08 +0530

    Time: 1945 ms (5.08%), Space: 16.2 MB (76.79%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -5,11 +5,9 @@ public:
         int ans = 0, divisiors = 1, sum = 1,  j = 1;
 
         for(int i=0; i<n; i++){
-            divisiors = 0;
-            sum = 0;
-            j=1;
-            // if(nums[i]%2 == 0) j = 2;
-            // else j = 3;
+            divisiors = 1;
+            sum = 1;
+            j=2;
             while(j<=nums[i]){
                 if(divisiors > 4 ) break;
                 if(nums[i]%j == 0){
