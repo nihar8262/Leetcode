a502fbb56aa3d4d379f7738351ea122e976cc4dc
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2026-04-24 16:30:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -8,14 +8,14 @@ public:
             if(moves[i] == 'L' || moves[i] == '_') length++;
             else length--;
         }
-        farthest = max(farthest, length);
+        farthest = max(farthest, abs(length));
         length = 0;
 
         for(int i = 0; i<n; i++){
             if(moves[i] == 'R' || moves[i] == '_') length++;
             else length--;
         }
-        farthest = max(farthest, length);
+        farthest = max(farthest, abs(length));
 
         return farthest;
     }
