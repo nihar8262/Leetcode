96e0496aa48b20a8dc35c161c7cdbdd175a67798
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2026-04-29 11:51:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.cpp
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int getMinDistance(vector<int>& nums, int target, int start) {
         int n = nums.size();
-        int ans = INT_MAX;
+        int ans = nums.size();
 
         for(int i = 0; i < n; i++){
             if(nums[i] == target) {
