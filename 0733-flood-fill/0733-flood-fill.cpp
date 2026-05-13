46323ef88c4af0d3557ca5c85896559ff44222b8
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2026-05-13 10:25:47 +0530

    Time: 0 ms (100%), Space: 18.3 MB (42.82%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -5,11 +5,11 @@ public:
         int n = image[0].size();
         int original = image[sr][sc];
 
+        if(original == color) return image;
+
         queue<pair<int, int>> q;
         q.push({sr, sc});
 
-        vector<vector<int>> vis(m, vector<int>(n,0));
-        vis[sr][sc] = color;
         image[sr][sc] = color;
 
         int delrow[] = {-1, 0, 1, 0};
@@ -26,8 +26,7 @@ public:
                 int ncol = c + delcol[i];
 
                 if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n
-                && vis[nrow][ncol] != color && image[nrow][ncol] == original){
-                    vis[nrow][ncol] = color;
+                 && image[nrow][ncol] == original){
                     image[nrow][ncol] = color;
                     q.push({nrow, ncol});
                 }
