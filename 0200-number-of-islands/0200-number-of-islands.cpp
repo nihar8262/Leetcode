81364dcda8a8a0a69782ac833b2e8d98b93c2566
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2026-05-14 14:57:41 +0530

    Time: 34 ms (17.36%), Space: 19.2 MB (35.25%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,38 +1,40 @@
 class Solution {
-
-private:
-    void dfs(int r, int c, vector<vector<char>>& grid, vector<vector<int>>& vis,
-             int delrow[], int delcol[]) {
-        vis[r][c] = 1;
-        int m = grid.size();
-        int n = grid[0].size();
-
-        for (int i = 0; i < 4; i++) {
-            int nrow = r + delrow[i];
-            int ncol = c + delcol[i];
-
-            if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n &&
-                !vis[nrow][ncol] && grid[nrow][ncol] == '1') {
-                dfs(nrow, ncol, grid, vis, delrow, delcol);
-            }
-        }
-    }
-
 public:
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         int ans = 0;
 
-        vector<vector<int>> vis(m, vector<int>(n, 0));
         int delrow[] = {-1, 0, 1, 0};
         int delcol[] = {0, 1, 0, -1};
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!vis[i][j] && grid[i][j] == '1') {
-                    dfs(i, j, grid, vis, delrow, delcol);
+        vector<vector<int>> vis(m, vector<int>(n,0));
+        queue<pair<int, int>> q;
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(!vis[i][j] && grid[i][j] == '1'){
                     ans++;
+                    q.push({i,j});
+
+                    while(!q.empty()){
+                        int r = q.front().first;
+                        int c = q.front().second;
+                        q.pop();
+
+                        for(int i = 0; i<4; i++){
+                            int nrow = r + delrow[i];
+                            int ncol = c + delcol[i];
+
+                            if(nrow>=0 && nrow<m &&
+                             ncol>=0 && ncol<n &&
+                             !vis[nrow][ncol] && 
+                             grid[nrow][ncol] == '1'){
+                                vis[nrow][ncol] = 1;
+                                q.push({nrow, ncol});
+                            }
+                        }
+                    }
                 }
             }
         }
