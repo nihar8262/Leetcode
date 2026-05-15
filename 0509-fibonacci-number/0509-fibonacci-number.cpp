b79b77664c30000d554c839ff9a6565dbcd57477
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2026-05-15 11:19:58 +0530

    Time: 0 ms (100%), Space: 7.6 MB (98.62%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,18 +1,17 @@
 class Solution {
 
-private:
-    int fibonacci(int n, vector<int>& dp){
-        if(n<=1) return n;
-
-        if(dp[n] != 0) return dp[n];
-
-        return dp[n] = fibonacci(n-1, dp) + fibonacci(n-2, dp);
-    }
-
 public:
     int fib(int n) {
-        vector<int> dp(n+1, 0);
+        int prev = 1, prev2 = 0;
 
-        return fibonacci(n, dp);
+        if(n<=1) return n;
+
+        for(int i = 2; i<=n; i++){
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return prev;
     }
 };
