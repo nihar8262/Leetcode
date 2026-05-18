7cdcbfb83ca4367c9368065dc53a41bee35499c8
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2026-05-18 13:49:23 +0530

    Time: 0 ms (100%), Space: 11.9 MB (28.91%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+
+private:
+    int countPath(int i, int j, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
+        if(i < 0 || j < 0) return 0;
+
+        if(obstacleGrid[i][j] == 1) return 0;
+
+        if(i == 0 && j == 0) return 1;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int up = countPath(i-1, j, obstacleGrid, dp);
+        int left = countPath(i, j-1, obstacleGrid, dp);
+
+        return dp[i][j] = up + left;
+    }
+
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+
+        return countPath(m-1, n-1, obstacleGrid, dp);
+    }
+};
