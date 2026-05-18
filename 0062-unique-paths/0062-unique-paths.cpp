7a257313a782147390b0df0b3118ea18122a3636
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2026-05-18 13:25:24 +0530

    Time: 3 ms (9.46%), Space: 9.3 MB (50.59%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,21 +1,21 @@
 class Solution {
-
-private:
-    int countMaxPath(int row, int col, vector<vector<int>> &dp){
-        if(row == 0 && col == 0) return 1;
-        else if(row < 0 || col < 0) return 0;
-
-        if(dp[row][col] != -1) return dp[row][col];
-
-        int u = countMaxPath(row-1, col, dp);
-        int l = countMaxPath(row, col-1, dp);
-
-        return dp[row][col] = l + u;
-    }
-
 public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n,-1)); 
-        return countMaxPath(m-1,n-1, dp);
+        
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(i == 0 && j == 0) dp[i][j] = 1;
+                else{
+                    int down = 0, right = 0;
+                    if(i>0) down = dp[i-1][j];
+                    if(j>0) right = dp[i][j-1];
+                    dp[i][j] = down + right;
+                }
+
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
