0e5ab83b7a9fd349f663d93d4624dc6f9bd8c304
Nihar chandra sharma <82151336+nihar8262@users.noreply.github.com>
2026-07-10 22:40:13 +0530

    Time: 0 ms (100%), Space: 8.9 MB (42.45%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        int b = 0, a = 0, l = 0, o = 0, n = 0;
+        vector<int> count(26, 0);
 
         for (char c : text) {
-            if (c == 'b') b++;
-            else if (c == 'a') a++;
-            else if (c == 'l') l++;
-            else if (c == 'o') o++;
-            else if (c == 'n') n++;
+            count[c - 'a']++;
         }
 
-        return min({b, a, l / 2, o / 2, n});
+        return min({
+            count['b' - 'a'],
+            count['a' - 'a'],
+            count['l' - 'a'] / 2,
+            count['o' - 'a'] / 2,
+            count['n' - 'a']
+        });
     }
 };
